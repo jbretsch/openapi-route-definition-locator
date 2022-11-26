c008c2b15aad2bb15a6ce05f1b4e557fef3e33d6
Jan Bretschneider <jan@bretti.net>
2022-11-26 22:36:23 +0100

    Dependency updates
    Spring Boot:    2.7.5 -> 2.7.6
    Swagger Parser: 2.1.3 -> 2.1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 defaultTasks("clean", "build")
 
 tasks.wrapper {
-    gradleVersion = "7.5.1"
+    gradleVersion = "7.6"
     distributionType = Wrapper.DistributionType.ALL
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.5")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.6")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
 }
