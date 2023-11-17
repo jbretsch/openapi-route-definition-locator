7620c95707a10b720a05ba15e9129e6ef9560e53
Jan Bretschneider <jan@bretti.net>
2023-11-17 18:21:32 +0100

    Set new SNAPSHOT version after release

--- buildSrc/src/main/kotlin/openapi-route-definition-locator.common-java-library.gradle.kts
+++ buildSrc/src/main/kotlin/openapi-route-definition-locator.common-java-library.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
 
 java {
     group = "net.bretti.openapi-route-definition-locator"
-    version = "0.6.6-sc-2022.0"
+    version = "0.6.7-sc-2022.0-SNAPSHOT"
     toolchain {
         // Keep the same Java compatibility as Spring Cloud Gateway.
         languageVersion.set(JavaLanguageVersion.of(17))
