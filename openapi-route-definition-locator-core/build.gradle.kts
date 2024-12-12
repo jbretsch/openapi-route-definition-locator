9c9c88ae290014d42e8cf3873640d40f3af3b69d
Jan Bretschneider <jan@bretti.net>
2024-12-12 07:28:26 +0100

    chore: Update dependencies
    Swagger Parser: 2.1.22 -> 2.1.24
    Lombok: 1.18.32 -> 1.18.36
    Gradle: 8.7 -> 8.11.1

--- openapi-route-definition-locator-core/build.gradle.kts
+++ openapi-route-definition-locator-core/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     compileOnly("io.micrometer:micrometer-core")
     implementation("org.springframework.cloud:spring-cloud-gateway-server")
     implementation("org.springframework:spring-webflux")
-    implementation("io.swagger.parser.v3:swagger-parser:2.1.22")
+    implementation("io.swagger.parser.v3:swagger-parser:2.1.24")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
