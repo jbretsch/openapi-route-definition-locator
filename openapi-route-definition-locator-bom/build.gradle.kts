e6dab44cd25b0174b158b65899f1c7ec1fcb04c2
Jan Bretschneider <jan@bretti.net>
2025-08-18 07:46:34 +0200

    Set new SNAPSHOT version after release

--- openapi-route-definition-locator-bom/build.gradle.kts
+++ openapi-route-definition-locator-bom/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
 javaPlatform {
     group = "net.bretti.openapi-route-definition-locator"
-    version = "1.1.0-sc-2024.0"
+    version = "1.1.1-sc-2024.0-SNAPSHOT"
 }
 
 publishing {
